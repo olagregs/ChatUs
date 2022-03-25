d509e59cf084745763a32731fb0f21ae9bb12ae5
Greg <gregoryflucena@gmail.com>
2022-03-25 20:23:07 -0300

    Project inicialization

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/client", (reques, response) => {
+  return response.status(200).json({
+    message: "Server is running"
+  });
+});
+
+app.listen(3333, () => console.log("Server is running at port 3333..."));
